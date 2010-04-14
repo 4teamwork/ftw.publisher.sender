4d594ecde9f0f267d605d09da95a01b86e0e6c7e
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-04-14 07:04:02 +0000

    fix move/rename base implementation

--- ftw/publisher/sender/eventhandlers.py
+++ ftw/publisher/sender/eventhandlers.py
@@ -6,7 +6,9 @@ def add_move_job(obj, event):
     This event handles move and rename jobs
     """
     if obj == event.object:
-        
+        # do nohting, if we are in portal_factory or the item is just created
+        if not data['oldParent'] or not getattr(event.object, '_at_creation_flag', False):
+            return        
         #set event info on on obj
         data = event.__dict__.copy()
         setattr(obj, 'event_information', data)
