4b71e46d01a484b42c4411d4b8373b1231d28d7a
Jonas Baumann <j.baumann@4teamwork.ch>
2010-08-17 16:29:18 +0000

    Added upgrade step for migrating the executed jobs storage to IOBTree

--- ftw/publisher/sender/upgrades.py
+++ ftw/publisher/sender/upgrades.py
@@ -1,9 +1,11 @@
-from zope.annotation.interfaces import IAnnotations
+from ftw.publisher.sender.interfaces import IQueue
 from transaction import savepoint
+from zope.annotation.interfaces import IAnnotations
+
 
 def RemoveAnnotations(portal_setup):
     plone = portal_setup.portal_url.getPortalObject()
-    
+
     annotations = IAnnotations(plone)
     if annotations.has_key('publisher-queue'):
         del annotations['publisher-queue']
@@ -11,5 +13,29 @@ def RemoveAnnotations(portal_setup):
         del annotations['publisher-realms']
     if annotations.has_key('publisher-dataFolder'):
         del annotations['publisher-dataFolder']
-    
+
     savepoint(1)
+
+
+def upgrade_executed_jobs_storage(portal_setup):
+    """The executed jobs storage has changed from PersistentList
+    to IOBTree storage, so we need to migrate the storage.
+
+    """
+    portal = portal_setup.portal_url.getPortalObject()
+    queue = IQueue(portal)
+    annotations = IAnnotations(portal)
+
+    # get jobs directly from the annotations - accessing with
+    # queue methods is not possible yet
+    jobs = list(annotations.get('publisher-executed', []))
+
+    # drop the current list
+    del annotations['publisher-executed']
+
+    # add every single job with the new methods
+    for job in jobs:
+        queue.append_executed_job(job)
+
+    # check if it worked
+    assert len(jobs) == queue.get_executed_jobs_length()
