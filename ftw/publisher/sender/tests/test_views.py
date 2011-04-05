695fb82d457508bc0f36377a51cf28222233b7df
Jonas Baumann <jone@jone.ch>
2011-04-05 23:18:08 +0200

    disabled formgen tests for now, installation in test setup fails

--- ftw/publisher/sender/tests/test_views.py
+++ ftw/publisher/sender/tests/test_views.py
@@ -8,7 +8,7 @@ from ftw.publisher.sender.persistence import Queue
 
 class TestPublishObject(PloneTestCase):
     layer = Layer
-    
+
     def afterSetUp(self):
         # add some default plone types
         testdoc2id = self.folder.invokeFactory('Document', 'test-doc-2')
@@ -20,7 +20,7 @@ class TestPublishObject(PloneTestCase):
         PublishObject(self.testdoc2,self.testdoc2.REQUEST)()
         self.assertEquals(queue.countJobs(),1)
         self.assertEquals(queue.getJobs()[0].action,'push')
-        
+
     def test_publish_plonesiteroot(self):
         self.assertEquals(isinstance(PublishObject(self.portal,self.portal.REQUEST), PublishObject), True)
 
@@ -45,20 +45,22 @@ class TestDeleteObject(PloneTestCase):
 
 
 
-class TestFormGenIntegration(PloneTestCase):
-    layer = Layer
+# XXX disabled for now, since installation of PloneFormGen seems
+# not to work properly.
+# class TestFormGenIntegration(PloneTestCase):
+#     layer = Layer
 
-    def afterSetUp(self):
-        #self.loginAsPortalOwner()
-        #import pdb; pdb.set_trace( )
-        self.folder.invokeFactory('FormFolder', 'ff1')
-        self.ff1 = getattr(self.folder, 'ff1')
+#     def afterSetUp(self):
+#         self.loginAsPortalOwner()
+#         # import pdb; pdb.set_trace( )
+#         self.folder.invokeFactory('FormFolder', 'ff1')
+#         self.ff1 = getattr(self.folder, 'ff1')
 
 
 
-    def test_publish_form(self):
+#     def test_publish_form(self):
 
-        self.assertEquals(1, 0)
+#         self.assertEquals(1, 0)
 
 
 
