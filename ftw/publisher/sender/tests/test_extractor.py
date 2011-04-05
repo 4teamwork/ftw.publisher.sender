f64234cda7e3d433b90b22af6254818b174e3bfa
Jonas Baumann <jone@jone.ch>
2011-04-05 22:42:17 +0200

    fixed some extractor tests according to code changes

--- ftw/publisher/sender/tests/test_extractor.py
+++ ftw/publisher/sender/tests/test_extractor.py
@@ -1,54 +1,67 @@
-import unittest
-from zope.component import getAdapters
-
-from ftw.publisher.core.interfaces import IDataCollector
-from zope.component import queryUtility
-
 from Products.PloneTestCase.ptc import PloneTestCase
-from ftw.publisher.sender.tests.layer import Layer
-
+from ftw.publisher.core.interfaces import IDataCollector
 from ftw.publisher.sender.extractor import Extractor
+from ftw.publisher.sender.tests.layer import Layer
+from zope.component import getAdapters
+import unittest
+
 
 class TestExtractor(PloneTestCase):
     layer = Layer
-    
+
     def afterSetUp(self):
         # add some default plone types
         testdoc1id = self.folder.invokeFactory('Document', 'test-doc-1')
         self.testdoc1 = getattr(self.folder,testdoc1id,None)
-        topicid = self.folder.manage_addProduct['ATContentTypes'].addATTopic(id='topic1',title='topic 1')
+        topicid = self.folder.manage_addProduct['ATContentTypes'].addATTopic(
+            id='topic1',title='topic 1')
         self.topic1 = getattr(self.folder, topicid, None)
         self.extractor = Extractor()
         self.extractor(self.testdoc1,'delete')
 
     def test_extractor_object(self):
         adapters = getAdapters((self.testdoc1,),IDataCollector)
-        expected_adapters = [u'field_data_adapter',u'properties_data_adapter',u'interface_data_adapter',u'portlet_data_adapter']
+        expected_adapters = [u'field_data_adapter',
+                             u'properties_data_adapter',
+                             u'backreferences_adapter',
+                             u'interface_data_adapter',
+                             u'portlet_data_adapter']
         list_adapters = []
         for name,adapter in adapters:
             list_adapters.append(name)
-        self.assertEquals(expected_adapters,list_adapters)
+        self.assertEquals(set(expected_adapters), set(list_adapters))
 
     def test_extractor_topic(self):
         adapters = getAdapters((self.topic1,),IDataCollector)
-        expected_adapters = [u'field_data_adapter',u'properties_data_adapter',u'topic_critera_adapter',u'interface_data_adapter',u'portlet_data_adapter']
+        expected_adapters = [u'field_data_adapter',
+                             u'properties_data_adapter',
+                             u'backreferences_adapter',
+                             u'topic_critera_adapter',
+                             u'interface_data_adapter',
+                             u'portlet_data_adapter']
         list_adapters = []
         for name,adapter in adapters:
             list_adapters.append(name)
-        self.assertEquals(expected_adapters,list_adapters)
+        self.assertEquals(set(expected_adapters), set(list_adapters))
 
     def test_getMetadata(self):
         metadata = self.extractor.getMetadata('delete')
-        uid=metadata['UID']
-        expected_metadata = {'UID': uid,
-                             'portal_type': 'Document',
-                             'physicalPath': '/Members/test_user_1_/test-doc-1', 
-                             'sibling_positions': {'topic1': 1, 'test-doc-1': 0},
-                             'action': 'delete',
-                             'review_state': 'private',
-                             'id': 'test-doc-1'
-                            }
-        self.assertEquals(expected_metadata,metadata)
+        # uid=metadata['UID']
+        expected_metadata = {'UID': 'a9e37ee08e61ce943ad074e6905b4004',
+         'portal_type': 'Document',
+         'modified': 'MODIFIED',
+         'physicalPath': '/Members/test_user_1_/test-doc-1',
+         'schema_path': 'Products.ATContentTypes.content.document.ATDocument.schema',
+         'sibling_positions': {'topic1': 1,
+                               'test-doc-1': 0},
+         'action': 'delete',
+         'review_state': 'private',
+         'id': 'test-doc-1'}
+
+        self.assertTrue('modified' in metadata)
+        metadata['modified'] = 'MODIFIED'
+
+        self.assertEquals(expected_metadata, metadata)
 
     def test_getRelativePath(self):
         path = self.extractor.getRelativePath()
