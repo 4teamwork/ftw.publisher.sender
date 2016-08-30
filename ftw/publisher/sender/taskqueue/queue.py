9bf8c87791b370151c88b42cf12a800260258e53
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2016-08-30 16:39:49 +0200

    Fix edge-case, if the published object is removed before the async worker collects the data from the object.

--- ftw/publisher/sender/taskqueue/queue.py
+++ ftw/publisher/sender/taskqueue/queue.py
@@ -1,8 +1,12 @@
 from collective.taskqueue import taskqueue
+from plone import api
 
 
 def enqueue_deferred_extraction(obj, action, filepath):
-    callback_path = '/'.join(obj.getPhysicalPath() +
+    callback_path = '/'.join(api.portal.get().getPhysicalPath() +
                              ('taskqueue_publisher_extract_object',))
+
+    path = '/'.join(obj.getPhysicalPath())
     taskqueue.add(callback_path, params={'action': action,
-                                         'filepath': filepath})
+                                         'filepath': filepath,
+                                         'path': path})
