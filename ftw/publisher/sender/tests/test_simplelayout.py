29e5204774ffb85dcf6ff121ada174bf303ce1b8
Martin Bächtold <martin@baechtold.me>
2018-02-22 08:30:01 +0100

    Remove class inheritance.
    I'm not sure if the "@skipIf" decorator on the super class also applies to the class inheriting from it.
    
    Also rename the classes to better reflect what they are testing.

--- ftw/publisher/sender/tests/test_simplelayout.py
+++ ftw/publisher/sender/tests/test_simplelayout.py
@@ -13,7 +13,7 @@ from Products.CMFCore.utils import getToolByName
 
 
 @skipIf(IS_AT_LEAST_PLONE_5_1, 'ftw.contentpage is not available for Plone 5')
-class TestPublishingSimplelayoutTypes(FunctionalTestCase):
+class TestPublishingFtwContentpageTypes(FunctionalTestCase):
 
     portal_type = 'ContentPage'
     page_builder = 'content page'
@@ -21,7 +21,7 @@ class TestPublishingSimplelayoutTypes(FunctionalTestCase):
     listingblock_builder = 'listing block'
 
     def setUp(self):
-        super(TestPublishingSimplelayoutTypes, self).setUp()
+        super(TestPublishingFtwContentpageTypes, self).setUp()
         self.grant('Manager')
 
         wftool = getToolByName(self.portal, 'portal_workflow')
@@ -57,12 +57,48 @@ class TestPublishingSimplelayoutTypes(FunctionalTestCase):
             ' in the queue.')
 
 
-class TestPublishingNEWSimplelayoutTypes(TestPublishingSimplelayoutTypes):
+class TestPublishingFtwSimplelayoutTypes(FunctionalTestCase):
 
     portal_type = 'ftw.simplelayout.ContentPage'
     page_builder = 'sl content page'
     textblock_builder = 'sl textblock'
     listingblock_builder = 'sl listingblock'
+
+    def setUp(self):
+        super(TestPublishingFtwSimplelayoutTypes, self).setUp()
+        self.grant('Manager')
+
+        wftool = getToolByName(self.portal, 'portal_workflow')
+        wftool.setChainForPortalTypes([self.portal_type],
+                                      'publisher-example-workflow')
+
+    @browsing
+    def test_blocks_are_published_with_contentpage(self, browser):
+        page = create(Builder(self.page_builder))
+        create(Builder(self.textblock_builder).within(page))
+
+        browser.login().visit(page)
+        Workflow().do_transition('publish')
+
+        queue = IQueue(self.portal)
+        self.assertEquals(
+            2, queue.countJobs(),
+            'Expected the page and the text block to be in the queue.')
+
+    @browsing
+    def test_sl_listing_block_publishes_its_children(self, browser):
+        page = create(Builder(self.page_builder))
+        listing_block = create(Builder(self.listingblock_builder).within(page))
+        create(Builder('file').within(listing_block))
+
+        browser.login().visit(page)
+        Workflow().do_transition('publish')
+
+        queue = IQueue(self.portal)
+        self.assertEquals(
+            3, queue.countJobs(),
+            'Expected the page, the listing block and the file to be'
+            ' in the queue.')
 
     def test_no_delete_jobs_for_blocks(self):
         """When a block is deleted, we do not want to instantly delete
