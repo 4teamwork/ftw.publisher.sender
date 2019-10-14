df0b03009cbc751ac7132ab72996e70a65a9df1e
Jonas Baumann <jone@jone.ch>
2019-10-14 14:27:54 +0200

    Refactor synchronizing simplelayout state so that it works for non-content-pages.

--- ftw/publisher/sender/browser/ftw_simplelayout.py
+++
@@ -1,9 +0,0 @@
-from ftw.publisher.sender.browser.views import PublishObject
-from ftw.simplelayout.configuration import synchronize_page_config_with_blocks
-
-
-class PublishSLContainer(PublishObject):
-
-    def __call__(self, *args, **kwargs):
-        synchronize_page_config_with_blocks(self.context)
-        return super(PublishSLContainer, self).__call__(*args, **kwargs)
