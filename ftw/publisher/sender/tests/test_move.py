5d4ea0279f739031175b5b5a4f697e29a8736ee6
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2021-07-19 12:06:55 -0400

    Create a move job upon folder_contents rename view (Plone 5 only).

--- ftw/publisher/sender/tests/test_move.py
+++ ftw/publisher/sender/tests/test_move.py
@@ -8,7 +8,10 @@ from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import statusmessages
 from ftw.testing import freeze
 from ftw.testing import staticuid
+from plone.protect.authenticator import createToken
+from plone.uuid.interfaces import IUUID
 from unittest import skipUnless
+from zope.component import getMultiAdapter
 import json
 
 
@@ -101,6 +104,60 @@ class TestMove(FunctionalTestCase):
                 u'utf8:newName': u'utf8:new_id',
                 u'utf8:newParent': u'utf8:/folder',
                 u'utf8:newTitle': u'unicode:The Page',
+                u'utf8:oldName': u'utf8:the-page',
+                u'utf8:oldParent': u'utf8:/folder'}}
+
+        if IS_AT_LEAST_PLONE_5_1:
+            expected[u'utf8:metadata'][u'utf8:modified'] = u'utf8:{}'.format(
+                str(page.modified()).decode('utf-8'))
+
+        self.assertEquals(
+            expected,
+            json.loads(data))
+
+    @browsing
+    @staticuid()
+    @skipUnless(IS_AT_LEAST_PLONE_5_1, 'object_rename only exists in Plone 5.x')
+    def test_renaming_object_via_folder_contents_rename_action(self, browser):
+
+        self.grant('Manager')
+        folder = create(Builder('folder').titled(u'Folder'))
+        with freeze(datetime(2018, 1, 2, 3, 4, 5)):
+            page = create(Builder('page').titled(u'The Page').within(folder))
+
+            self.assertEquals(0, IQueue(self.portal).countJobs())
+
+            form_data = {}
+            form_data['_authenticator'] = createToken()
+            form_data['UID_1'] = IUUID(page)
+            form_data['newid_1'] = 'new_id'
+            form_data['newtitle_1'] = u'Ch\xe4nged title'
+
+            browser.login().visit(folder, view='@@fc-rename', data=form_data)
+
+        self.assertEquals(1, IQueue(self.portal).countJobs())
+
+        job, = IQueue(self.portal).getJobs()
+        self.assertEquals('move', job.action)
+        data = job.getData()
+        self.assertTrue(data)
+
+        self.maxDiff = None
+
+        expected = {
+            u'utf8:metadata': {
+                u'utf8:UID': u'utf8:testrenamingobjectviafolde000002',
+                u'utf8:action': u'utf8:move',
+                u'utf8:id': u'utf8:new_id',
+                u'utf8:modified': u'utf8:2018/01/02 03:04:05 GMT+1',
+                u'utf8:physicalPath': u'utf8:/folder/new_id',
+                u'utf8:portal_type': u'utf8:Document',
+                u'utf8:review_state': u'utf8:',
+                u'utf8:sibling_positions': {u'utf8:new_id': 0}},
+            u'utf8:move': {
+                u'utf8:newName': u'utf8:new_id',
+                u'utf8:newParent': u'utf8:/folder',
+                u'utf8:newTitle': u'unicode:Ch\xe4nged title',
                 u'utf8:oldName': u'utf8:the-page',
                 u'utf8:oldParent': u'utf8:/folder'}}
 
